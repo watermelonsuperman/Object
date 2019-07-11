d17367d838346e8d63cf583f8ef46a92683a1f4a
watermelonsuperman <49156515+watermelonsuperman@users.noreply.github.com>
2019-07-11 17:36:41 +0800

    Create Object.js

---
+++ Object.js
@@ -0,0 +1,21 @@
+var user = {};
+user.name = "John";
+console.log(user);
+user.surname = "Mike";
+console.log(user);
+user.name = "Peter";
+console.log(user);
+user={
+	surname : "Mike"
+};
+console.log(user);
+
+
+
+var fruit = {
+apple: 20,
+pear: 20,
+peach: 10
+};
+var sum = fruit.apple + fruit.pear + fruit.peach;
+console.log(sum);
